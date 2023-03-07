e85a5109f0f06fb8cfc70a1317fdf89f96bcdee0
Luca Holzer <luca.holzer@knapp.com>
2023-03-07 13:05:49 +0100

    removed main.py

--- DjangoGirlsBlog/blog/forms.py
+++ DjangoGirlsBlog/blog/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 from .models import Post
 
-class PostForm(forms.ModelForm):
 
+class PostForm(forms.ModelForm):
     class Meta:
          model = Post
          fields = ('title', 'text',)
